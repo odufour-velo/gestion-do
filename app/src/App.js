71855e8680ff1825c2954440ca537b4654f03158
Olivier DUFOUR <odufour.dev@gmail.com>
2026-01-17 18:47:33 +0100

    feat: Improve the submit process with messages

--- app/src/App.js
+++ app/src/App.js
@@ -1,3 +1,11 @@
+function doGet() {
+    const template = HtmlService.createTemplateFromFile('Web/Form');
+  return template.evaluate()
+      .setTitle('Comité Auvergne-Rhône-Alpes de Cyclisme - Saisie des épreuves')
+      .setWidth(850)
+      .setHeight(700);
+}
+
 /**
  * Create the menu when sheet is opened
  */
@@ -11,7 +19,7 @@ function onOpen() {
  * Configure and display the form in a modal dialog
  */
 function showForm() {
-    
+
   const html = HtmlService.createTemplateFromFile('Web/Form');
   
   const interface = html.evaluate()
